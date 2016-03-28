f4bb3bd23998c9834ade1dfaa16d334d43222591
Ashley Maceli and Noah Kittleson <ashley@>
2016-03-28 11:21:14 -0700

    made grocery sorting and uppercasing

---
+++ groceries/js/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $("#blanks form").submit(function(event) {
+
+    var groceries = ["item1", "item2", "item3", "item4"];
+
+    var inputs = groceries.map(function(grocery) {
+      return $("#" + grocery).val().toUpperCase();
+    });
+    inputs.sort();
+    inputs.forEach(function(input) {
+      if (input) {
+        $(".output").append("<li>" + input + "</li>");
+      }
+    });
+    event.preventDefault();
+  });
+});
